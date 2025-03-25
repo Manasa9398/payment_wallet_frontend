8ca1d6d27e4341c112acb90f103a62a1e4e43a9e
Manasa Goud <manasabhimagani@gmail.com>
2025-03-25 18:11:06 +0530

    Update main.jsx

--- Payment_wallet/src/main.jsx
+++ Payment_wallet/src/main.jsx
@@ -1,21 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import Login from './Components/Login.jsx'
-// import Register from './Components/Register.jsx'
-// import Welcome from './Components/Welcome.jsx'
-// import Counter from './Components/Counter.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//     <Login/>
-//     <Register/>
-//     <Welcome/>
-//     <Counter/>
-//   </StrictMode>,
-// )
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
