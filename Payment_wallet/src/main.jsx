c0d4844084ac34e2f7a40013dc46933bca4e1f81
Manasa Goud <manasabhimagani@gmail.com>
2025-03-31 15:20:35 +0530

    Update main.jsx

--- Payment_wallet/src/main.jsx
+++ Payment_wallet/src/main.jsx
@@ -4,16 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
-import Welcome from "./Components/Welcome"; 
+import Welcome from "./Components/Welcome";
+import Counter from "./Components/Counter";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        {/* <Route path="/Login" element={<Login />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Welcome name="User" />} />
+        <Route path="/Welcome" element={<Welcome name="User" />} />
+        <Route path="/Counter" element={<Counter />} />
+
       </Routes>
     </Router>
   </React.StrictMode>
